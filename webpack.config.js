cd6b02e6ef33be3f1decfa6900fe3e5c7db6e3b6
jessephilip <jessephilip@gmail.com>
2017-03-05 23:13:29 -0600

    initial push

---
+++ webpack.config.js
@@ -0,0 +1,1 @@
+module.exports={entry:"./view/assets/javascript/master",output:{filename:"bundle.js",path:__dirname},module:{rules:[{enforce:"pre",test:/\.js$/,loader:"source-map-loader"},{enforce:"pre",test:/\.tsx?$/,use:"source-map-loader"},{test:/\.tsx?$/,loader:"ts-loader",exclude:/node_modules/}]},resolve:{extensions:[".tsx",".ts",".js"]},devtool:"inline-source-map"};
